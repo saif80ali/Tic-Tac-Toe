78f5cb615f8c1e5f74bf7b07d3af31ba25273151
saif80ali <mdsaif80ali@gmail.com>
2022-05-17 20:57:08 +0530

    last commit

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-let player1 = "";
-let player2 = "";
-let numberOfTimesClicked = 0;
-let mode = "";
-let turn = "X";
-let myboxes = document.getElementsByClassName("boxes")
+var player1 = "";
+var player2 = "";
+var numberOfTimesClicked = 0;
+var mode = "";
+var turn = "X";
+var draw = 0;
+var startTime = 0;
+var endTime = 0;
+var myflag = 0;
+var myboxes = document.getElementsByClassName("boxes");
 const playerTurn = new Audio("./assets/playerTurn.mp3");
 const gameOver = new Audio("./assets/gameover.mp3");
+document.getElementById("todayDate").innerHTML = new Date().toDateString()
 
-
-function ComputersTurn (min=0,max=8){
+function ComputersTurn(min = 0, max = 8) {
   min = Math.ceil(min);
   max = Math.floor(max);
   val = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  if(myboxes[val].querySelector(".boxtext").innerText === ""){
-    myboxes[val].querySelector(".boxtext").innerText = turn
+  if (myboxes[val].querySelector(".boxtext").innerText === "") {
+    myboxes[val].querySelector(".boxtext").innerText = turn;
     numberOfTimesClicked++;
-    checkwin()
-    turn = changeTurn()
+    checkwin();
+    console.log("computer call kiya")
+    turn = changeTurn();
+  } else {
+    ComputersTurn();
   }
-  else{
-    ComputersTurn()
-  }
-  
-
 }
 
 function showWinnerModal(msg, imagepath) {
@@ -39,32 +41,41 @@ function showWinnerModal(msg, imagepath) {
   }, 200);
 }
 
-function updateScores(player1, player2) {
+function updateScores(player1, player2, timeTaken1) {
+  let timeTaken = timeTaken1 - startTime;
+  timeTaken /= 1000;
+  seconds = Math.round(timeTaken);
   var score1 = 0;
   var score2 = 0;
-  let tbody = document.getElementById("tbody");
-  var row = tbody.insertRow();
+  let mytbody = document.getElementById("mytbody");
+  var row = mytbody.insertRow();
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
 
   cell1.innerHTML = player1;
   cell2.innerHTML = player2;
+  cell3.innerHTML = seconds + " seconds";
 
-  var table = document.getElementById("tbody");
-  for (var i = 0, row; (row = table.rows[i]); i++) {
-    for (var j = 0, col; (col = row.cells[j]); j++) {
+  let tableofpoints = document.getElementById("mytbody")
+  for (let i = 0, row; (row = tableofpoints.rows[i]); i++) {
+    for (let j = 0, col; (col = row.cells[j]); j++) {
       if (j === 0) {
         score1 = parseInt(col.innerText) + score1;
-      } else {
+      } else if(j===1) {
         score2 = parseInt(col.innerText) + score2;
       }
     }
   }
-  document.getElementById("total1").innerHTML = score1
-  document.getElementById("total2").innerHTML = score2
+  document.getElementById("total1").innerHTML = score1;
+  document.getElementById("total2").innerHTML = score2;
+  document.getElementById("draw").innerHTML = draw;
+  document.getElementById("toalgames").innerHTML = draw + score1 + score2;
 }
 
 function readyToPlay() {
+  myflag = 0
+  startTime = new Date();
   turn = "X";
   numberOfTimesClicked = 0;
   document.getElementById("x-turn").classList.add("turn-border");
@@ -74,6 +85,7 @@ function readyToPlay() {
 }
 
 function resetBoxes() {
+  startTime = new Date();
   let gameboxes = document.getElementsByClassName("boxtext");
   Array.from(gameboxes).forEach((element) => {
     element.innerHTML = "";
@@ -101,20 +113,37 @@ function checkwin() {
       let winner = boxtext[e[0]].innerText;
       if (winner == "X") {
         winner = player1;
-        updateScores(1, 0);
-      } else {
+        endTime = new Date();
+        
+        updateScores(1, 0,endTime);
+        console.log("Player 1 ka update")
+      } 
+      else {
         winner = player2;
-        updateScores(0, 1);
+        endTime = new Date();
+
+        updateScores(0, 1, endTime);
+        
+        console.log("Player 2 ka update")
       }
       showWinnerModal(winner + " wins !", "./assets/winner.gif");
-    } else if (numberOfTimesClicked >= 9) {
+      myflag = 1
+
+    } 
+      else if (numberOfTimesClicked >= 9){
       showWinnerModal("It is a Draw!", "./assets/draw.gif");
-      updateScores(1, 1);
+      draw++;
+      endTime = new Date();
+
+      updateScores(0, 0,endTime);
+      myflag = 1
+      console.log("Draw ka update")
     }
   });
 }
 
 function changeTurn() {
+  
   tempTurn = turn === "X" ? "O" : "X";
   if (tempTurn === "X") {
     document.getElementById("x-turn").classList.add("turn-border");
@@ -135,16 +164,24 @@ function askplayername(secondplayer) {
     player2 = "Computer";
     mode = "computer";
   }
+  myflag = 0
+  draw = 0;
+  startTime = new Date();
   resetBoxes();
-  document.getElementById("total1").innerHTML = ""
-  document.getElementById("total2").innerHTML = ""
-  document.getElementById("player1Name").innerHTML = player1;
-  document.getElementById("player2Name").innerHTML = player2;
-  document.getElementById("tbody").innerHTML = "";
-  document.getElementById("player1").innerText = player1;
-  document.getElementById("player2").innerText = player2;
+  document.getElementById("total1").innerHTML = "";
+  document.getElementById("total2").innerHTML = "";
+  document.getElementById("draw").innerHTML = "";
+  var playername1 = document.getElementsByClassName("playername1");
+  var playername2 = document.getElementsByClassName("playername2");
+  for (let i = 0, len = playername1.length; i < len; i++) {
+    playername1[i].innerHTML = player1;
+  }
+  for (let i = 0, len = playername2.length; i < len; i++) {
+    playername2[i].innerHTML = player2;
+  }
+  document.getElementById("mytbody").innerHTML = ""
   document.getElementById("norecords").style.display = "none";
-  document.getElementById("tbody").classList.remove("d-none");
+  document.getElementById("mytbody").classList.remove("d-none");
 }
 
 let boxes = document.getElementsByClassName("boxes");
@@ -155,10 +192,16 @@ Array.from(boxes).forEach((element) => {
       boxtext.innerText = turn;
       playerTurn.play();
       numberOfTimesClicked++;
-      checkwin();
+      if(mode != "computer"){
+        checkwin();
+      }
+      console.log("player call kiya")
       turn = changeTurn();
-      if(mode == "computer"){
-        ComputersTurn()
+      if (mode == "computer") {
+        checkwin()
+        if(myflag!=1){
+          ComputersTurn();
+        }
       }
     }
   });
